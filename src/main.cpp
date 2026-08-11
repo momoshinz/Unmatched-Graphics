70cd87c08652284cecf6ed9b528663a91ff0b9d9
momoshinz <danyled1993@gmail.com>
2026-08-12 02:53:50 +0330

    working on main menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
-    const int screenWidth = 1365;
-    const int screenHeight = 767;
+    const int screenWidth = 1302;
+    const int screenHeight = 703;
 
     InitWindow(
         screenWidth,
